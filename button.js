f467fe5e42ede81632f9260dbf58671b2c052398
BZerefa <zereb525@newschool.edu>
2024-11-18 21:03:36 -0500

    update index and place

--- button.js
+++ button.js
@@ -1,10 +1,12 @@
 console.log("file is working");
 
 let backgroundButton = document.getElementById("button");
-let websiteLink = document.getElementById("link");
+let cloudLink = document.getElementById("cloud");
+let gridLink = document.getElementById("links");
 
 backgroundButton.addEventListener("click", function (event) {
   console.log("clicked");
   document.body.classList.toggle("button-active");
-  websiteLink.classList.toggle("link-active");
+  cloudLink.classList.toggle("cloud-active");
+  gridLink.classList.toggle("links-active");
 });
